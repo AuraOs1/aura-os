23070beddd3068a1d704a95e414f541a5a0e05e4
AuraOs1 <auraos.founder@gmail.com>
2026-08-01 22:22:27 +0530

    Upload src/app/page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import DashboardLayout from "./(dashboard)/layout";
+import DashboardPage from "./(dashboard)/page";
+
+export default function RootPage() {
+  return (
+    <DashboardLayout>
+      <DashboardPage />
+    </DashboardLayout>
+  );
+}
